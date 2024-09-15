eecfb5f55d41be28524f42187e84f6d94f1dece2
Alexander Kholodovitch <kholodovitch@gmail.com>
2024-09-15 12:53:31 +0300

    Create SQL scripts by String.format.

--- src/main/java/ek/declaration/services/DeclarationService.java
+++ src/main/java/ek/declaration/services/DeclarationService.java
@@ -25,9 +25,9 @@ public class DeclarationService {
 
         for (Nomenklature nomenklature : nomenclatureList) {
             //вставка новых данных из запроса
-            String selectSql = "insert into  nomenklatura_ (ARTICLE,NAME,NALOG,MESABBREV,PXT3,COD_PROD,PRODUCER) " +
-                    "values ('"+ nomenklature.getArtcent() + "', '" + nomenklature.getName() + "', "+ nomenklature.getVat() +", '" + nomenklature.getMeasabbr()
-                    + "', '"+ nomenklature.getPht3() + "', '" + nomenklature.getCod_prod() + "', '" + nomenklature.getProducer() + "');";
+            String selectSql = String.format("insert into  nomenklatura_" +
+                    " (ARTICLE, NAME, NALOG, MESABBREV, PXT3, COD_PROD, PRODUCER) values ('%s', '%s', %s, '%s', '%s', '%s', '%s');",
+                    nomenklature.getArtcent(), nomenklature.getName(), nomenklature.getVat(), nomenklature.getMeasabbr(), nomenklature.getPht3(), nomenklature.getCod_prod(), nomenklature.getProducer());
             System.out.println(selectSql);
             statement.execute(selectSql);
             System.out.println("statement.execute(selectSql);");
@@ -41,9 +41,9 @@ public class DeclarationService {
 
         for (Storage storage : storageList) {
             //вставка новых данных из запроса
-            String selectSql = "insert into  sclady_ (LOC,LOCINFO,[GROUPING],INN,KPP,STATE,ADDRESS) " +
-                    "values ('"+ storage.getLoc() + "', '" + storage.getLocinfo() + "', '"+ storage.getGrouping() +"', '" + storage.getInn()
-                    + "', '"+ storage.getKpp() + "', " + storage.getState() + ", '" + storage.getAddr() + "');";
+            String selectSql = String.format("insert into  sclady_" +
+                    " (LOC, LOCINFO, [GROUPING], INN, KPP, STATE, ADDRESS) values ('%s', '%s', '%s', '%s', '%s', %d, '%s');",
+                    storage.getLoc(), storage.getLocinfo(), storage.getGrouping(), storage.getInn(), storage.getKpp(), storage.getState(), storage.getAddr());
             System.out.println(selectSql);
             statement.execute(selectSql);
             System.out.println("statement.execute(selectSql);");
@@ -57,8 +57,9 @@ public class DeclarationService {
 
         for (Suppliers suppliers : suppliersList) {
             //вставка новых данных из запроса
-            String selectSql = "insert into  postavchiki_ (ID,INN,KPP,NAMEP) " +
-                    "values ('"+ suppliers.getId()+ "', '" + suppliers.getInn() + "', '"+ suppliers.getKpp() +"', '" + suppliers.getNamep() + "');";
+            String selectSql = String.format("insert into  postavchiki_" +
+                    " (ID, INN, KPP, NAMEP) values ('%s', '%s', '%s', '%s');",
+                    suppliers.getId(), suppliers.getInn(), suppliers.getKpp(), suppliers.getNamep());
             System.out.println(selectSql);
             statement.execute(selectSql);
             System.out.println("statement.execute(selectSql);");
@@ -72,21 +73,20 @@ public class DeclarationService {
 
         for (Receipts receipts : receiptsList) {
             //вставка новых данных из запроса
-            String selectSql = "insert into  postupleniya_ (NOMER,[DATA],LOC,CLIENTID,INN,KPP,NACLPOST,CHFPOST,DATACHF,"
-                              + "ARTICLE,KOLVO,ITPRNOTAX,TOTPRNOTAX,ITPRICE,TOTPRICE,TAXRATE,TAXSUM,OPCODE,PRODUCER,USEROP,LOCFROM,RECIVER) "
-                              +  "values ('"+ receipts.getNomer() + createDateString(receipts.getDt()) + receipts.getLoc() +"', '" + receipts.getClientid()
-                              + "', '" + receipts.getInn() +"', '" + receipts.getKpp() + "', '" + receipts.getNaclpost() + "', '"+ receipts.getChfpost() + createDateString(receipts.getDatachf())
-                              + receipts.getArticle() +"', '" + receipts.getKolvo() + "', '" + receipts.getItprnotax() + "', '"+ receipts.getTotprnotax()
-                              + "', '"+ receipts.getItprice() +"', '" + receipts.getTotprice() + "', '" + receipts.getTaxrate() + "', '"+ receipts.getTaxsum()
-                              + "', '"+ receipts.getOpcode() +"', '" + receipts.getProducer() + "', '" + receipts.getUserop() + "', '"+ receipts.getLocfrom() + "', '"+ receipts.getReciver() + "');";
+            String selectSql = String.format("insert into  postupleniya_" +
+                    " (NOMER, [DATA], LOC, CLIENTID, INN, KPP, NACLPOST, CHFPOST, DATACHF, ARTICLE, KOLVO, ITPRNOTAX, TOTPRNOTAX, ITPRICE, TOTPRICE, TAXRATE, TAXSUM, OPCODE, PRODUCER, USEROP, LOCFROM, RECIVER)" +
+                    " values ('%s%s%s', '%s', '%s', '%s', '%s', '%s%s%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%d', '%s', '%s', '%s', '%s');",
+                    receipts.getNomer(), createDateString(receipts.getDt()), receipts.getLoc(), receipts.getClientid(), receipts.getInn(), receipts.getKpp(), receipts.getNaclpost(), receipts.getChfpost(),
+                    createDateString(receipts.getDatachf()), receipts.getArticle(), receipts.getKolvo(), receipts.getItprnotax(), receipts.getTotprnotax(), receipts.getItprice(), receipts.getTotprice(),
+                    receipts.getTaxrate(), receipts.getTaxsum(), receipts.getOpcode(), receipts.getProducer(), receipts.getUserop(), receipts.getLocfrom(), receipts.getReciver());
             System.out.println(selectSql);
             statement.execute(selectSql);
             System.out.println("statement.execute(selectSql);");
         }
     }
 
-    public String createDateString(String dateString){
-        if(dateString == null){
+    public String createDateString(String dateString) {
+        if (dateString == null) {
             return "', null, '";
         } else
             return "', CAST('" + dateString + "' as datetime), '";
@@ -99,9 +99,10 @@ public class DeclarationService {
 
         for (Ostatki ostatki : ostatkiList) {
             //вставка новых данных из запроса
-            String selectSql = "insert into  ostatki_ (LOC,ARTICLE,LASTPRICEN,OSTNACH,OSTKONEC,SDATE,CDATE) " +
-                    "values ('"+ ostatki.getLoc() + "', '" + ostatki.getArticle() + "', "+ ostatki.getLastpricen() +", " + ostatki.getOstnach()
-                    + ", "+ ostatki.getOstkonec() + ", CAST('" + ostatki.getSdate() + "' as datetime), CAST('" + ostatki.getCdate() + "' as datetime));";
+            String selectSql = String.format("insert into  ostatki_" +
+                    " (LOC, ARTICLE, LASTPRICEN, OSTNACH, OSTKONEC, SDATE, CDATE)" +
+                    " values ('%s', '%s', %s, %s, %s, CAST('%s' as datetime), CAST('%s' as datetime));",
+                    ostatki.getLoc(), ostatki.getArticle(), ostatki.getLastpricen(), ostatki.getOstnach(), ostatki.getOstkonec(), ostatki.getSdate(), ostatki.getCdate());
             System.out.println(selectSql);
             statement.execute(selectSql);
             System.out.println("statement.execute(selectSql);");
@@ -115,9 +116,10 @@ public class DeclarationService {
 
         for (Sales sales : salesList) {
             //вставка новых данных из запроса
-            String selectSql = "insert into  prodazhy_ (loc,article,kolvo,totprice,taxsum,taxrate,sdate,cdate) " +
-                    "values ('"+ sales.getLoc() + "', '" + sales.getArticle() + "', '"+ sales.getKolvo() +"', '" + sales.getTotprice()
-                    + "', '"+ sales.getTaxsum() + "', '"+ sales.getTaxrate() + "', CAST('" + sales.getSdate() + "' as datetime), CAST('" + sales.getCdate() + "' as datetime));";
+            String selectSql = String.format("insert into  prodazhy_" +
+                    " (loc, article, kolvo, totprice, taxsum, taxrate, sdate, cdate)" +
+                    " values ('%s', '%s', '%s', '%s', '%s', '%s', CAST('%s' as datetime), CAST('%s' as datetime));",
+                    sales.getLoc(), sales.getArticle(), sales.getKolvo(), sales.getTotprice(), sales.getTaxsum(), sales.getTaxrate(), sales.getSdate(), sales.getCdate());
             System.out.println(selectSql);
             statement.execute(selectSql);
             System.out.println("statement.execute(selectSql);");
